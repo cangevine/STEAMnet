be73145d27377cb5bcb6f8589804d535e44ee7b3
Colin Angevine <cangevine@friendscentral.org>
2012-10-24 21:53:07 -0400

    Basic placeholders set up for sparks, projects, and homepage

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/test', routes.test)
-app.get('/users', user.list);
+app.get('/sparks', routes.sparks)
+app.get('/projects', routes.projects);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ exports.index = function(req, res){
   res.render('index', { title: 'STEAMnet' });
 };
 
-exports.test = function(req, res) {
-	res.render('test', {title: 'Sample STEAMnet page'});
+exports.sparks = function(req, res) {
+	res.render('sparks', {title: 'Sparks: Ideas and inspriation'});
+}
+
+exports.projects = function(req, res) {
+	res.render('projects', {title: 'Projects: The ridiculous things STEAM might actually try to do...'});
 }
