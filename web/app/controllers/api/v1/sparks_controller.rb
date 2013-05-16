2e72b97389e95da2f782eb5a858207711c5695e8
Max Luzuriaga <max@luzuriaga.com>
2013-05-16 11:19:15 -0300

    Added user association to sparks creation
    Also added xml formats, commented out in case json parsing epic fails

--- web/app/controllers/api/v1/sparks_controller.rb
+++ web/app/controllers/api/v1/sparks_controller.rb
@@ -58,13 +58,18 @@ class Api::V1::SparksController < ApplicationController
       end
     end
     
+    user = User.find(:name => params[:username])
+    user.sparks << @spark
+    
     respond_to do |format|
       if @spark.new_record?
         format.html { redirect_to @spark, :notice => 'Spark was successfully created.' }
         format.json { render :json => @spark, :status => :created, :location => @spark }
+        #format.xml { render :xml => @spark, :status => :created, :location => @spark }
       else
         format.html { render :action => "new" }
         format.json { render :json => @spark.errors, :status => :unprocessable_entity }
+        #format.xml { render :xml => @spark.errors, :status => :unprocessable_entity }
       end
     end
   end
