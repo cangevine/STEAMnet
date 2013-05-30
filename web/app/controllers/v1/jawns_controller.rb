18661521a0fa56d9159e165bafba5a1c86667b92
Max Luzuriaga <max@luzuriaga.com>
2013-05-30 18:27:13 -0400

    Added /api/v1/jawns functionality. Should return the most recent Sparks and Ideas together in one array.

--- web/app/controllers/v1/jawns_controller.rb
+++ web/app/controllers/v1/jawns_controller.rb
@@ -1,2 +1,23 @@
 class V1::JawnsController < ApplicationController
+  
+  # GET /jawns
+  # GET /jawns.json
+  def index
+    if params[:limit]
+      sparks = Spark.order("id DESC").limit(params[:limit])
+      ideas = Idea.order("id DESC").limit(params[:limit])
+      
+      @jawns = (sparks + ideas).sort_by(&:created_at).reverse.take(params[:limit].to_i) 
+    else
+      sparks = Spark.order("id DESC")
+      ideas = Idea.order("id DESC")
+      
+      @jawns = (sparks + ideas).sort_by(&:created_at).reverse
+    end
+    
+    respond_to do |format|
+      format.json { render :json => @jawns }
+    end
+  end
+  
 end
