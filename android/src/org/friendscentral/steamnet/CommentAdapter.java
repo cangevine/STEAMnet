c71549408b161de4636e0c870112f6c7ed7ec676
Aqeel Phillips <aphillips@friendscentral.org>
2013-06-29 19:30:38 +0100

    Comment baseclass

--- android/src/org/friendscentral/steamnet/CommentAdapter.java
+++ android/src/org/friendscentral/steamnet/CommentAdapter.java
@@ -1,11 +1,39 @@
 package org.friendscentral.steamnet;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CommentAdapter extends BaseAdapter {
-
+	Comment[] comments;
+	Context context;
+	
+	public CommentAdapter(Context c, Comment[] com) {
+		context = c;
+		comments = com;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LinearLayout comment = new LinearLayout(context);
+		String username = comments[position].getUser();
+		String commentContent = comments[position].getContent();
+		
+		TextView user = new TextView(context);
+		user.setTextSize(20);
+		user.setText(username);
+		TextView content = new TextView(context);
+		content.setText(commentContent);
+		
+		comment.addView(user);
+		comment.addView(content);
+		
+		return comment;
+	}
+	
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
@@ -22,12 +50,6 @@ public class CommentAdapter extends BaseAdapter {
 	public long getItemId(int arg0) {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
